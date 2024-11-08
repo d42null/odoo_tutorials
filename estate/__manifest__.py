9e38d62c4c530b28e7b5c8e033c7775f8beeca22
Danylo Palianychka <d152null@gmail.com>
2024-11-08 11:30:21 +0200

    after 4 lesson

---
+++ estate/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "RealEstate",
+
+    'summary': """
+        RealEstate
+    """,
+
+    'description': """
+        RealEstate
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials/RealEstate',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+
+    'data': ['security/ir.model.access.csv',],
+    # 'assets': {
+    #     'web.assets_backend': [
+    #         'awesome_clicker/static/src/**/*',
+    #     ],
+
+    # },
+    'license': 'AGPL-3'
+}
